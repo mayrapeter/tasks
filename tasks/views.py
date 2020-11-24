44ba9a1118cbf7657675c880f38bd5868a1ebc56
Mayra Peter <mayra11peter@gmail.com>
2020-11-24 15:30:36 -0300

    arrumando para o client

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .serializers import TaskSerializer
+from django.core import serializers
 
 
 # Create your views here.
@@ -7,3 +9,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+def tasks(request):
+    tasks = Task.objects.all()
+    tasks_json = serializers.serialize("json", tasks)
+    return HttpResponse(jsontasks_jsontask ,content_type="application/json")
+
+def add(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return HttpResponse(serializer.data, status=201)
+        
+    return HttpResponse(serializer.errors, status=404)
