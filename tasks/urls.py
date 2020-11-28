2326a7fda2f85e0c4fc929c07eed4219d0148c5d
mayrapeter <38528459+mayrapeter@users.noreply.github.com>
2020-11-28 00:16:23 -0300

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('get', views.tasks, name='tasks'),
     path('add', views.add, name='add'),
+    path('delete', views.delete, name='delete'),
 ]
