7f583366aed279b9fd71fa598245a8e282342814
mayrapeter <38528459+mayrapeter@users.noreply.github.com>
2020-11-27 23:30:21 -0300

    Update views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .serializers import TaskSerializer
 from django.core import serializers
 from .models import Task
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 
 
 # Create your views here.
@@ -11,15 +13,17 @@ from .models import Task
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
 
+@api_view(["GET"])
 def tasks(request):
     tasks = Task.objects.all()
     tasks_json = serializers.serialize("json", tasks)
     return HttpResponse(tasks_json ,content_type="application/json")
 
+@api_view(["POST"])
 def add(request):
-    serializer = TaskSerializer(data=request.data)
+    dataJson = JSONParser().parse(request)
+    serializer = TaskSerializer(data=dataJson)
     if serializer.is_valid():
         serializer.save()
-        return HttpResponse(serializer.data, status=201)
-        
-    return HttpResponse(serializer.errors, status=404)
+        return JsonResponse(serializer_class.data, status=201)
+    return JsonResponse(serializer_class.errors, status=400)
